d5130b310fde2ea27a8741f706f1c4deda6d9bbe
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-09-06 02:46:24 +0900

    Modify activity structure

--- app/src/main/java/com/kwang0/introduce/layout/main/MainActivity.kt
+++ app/src/main/java/com/kwang0/introduce/layout/main/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.kwang0.introduce
+package com.kwang0.introduce.layout.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kwang0.introduce.R
 
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/kwang0/introduce/layout/splash/SplashActivity.kt
+++ app/src/main/java/com/kwang0/introduce/layout/splash/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.kwang0.introduce
+package com.kwang0.introduce.layout.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kwang0.introduce.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
