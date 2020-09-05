e28445d43cb5f07c59de7af7abd50c4955d2f265
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-09-06 02:50:21 +0900

    Add kwang0 logo view

---
+++ app/src/main/java/com/kwang0/introduce/view/kwang0/Kwang0View.kt
@@ -0,0 +1,13 @@
+package com.kwang0.introduce.view.kwang0
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class Kwang0View @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+    init {
+
+    }
+}
