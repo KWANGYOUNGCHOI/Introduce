cf4d2039ceadc3e907eca754692ed4ee1b79364d
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-09-09 23:19:59 +0900

    Add splash post delay

--- app/src/main/java/com/kwang0/introduce/utils/HandlerUtils.kt
+++ app/src/main/java/com/kwang0/introduce/utils/HandlerUtils.kt
@@ -1,4 +1,9 @@
 package com.kwang0.introduce.utils
 
+import android.os.Handler
+
 object HandlerUtils {
+    fun splashPostDelay(cb: () -> Unit) {
+        Handler().postDelayed(cb, 1500)
+    }
 }
