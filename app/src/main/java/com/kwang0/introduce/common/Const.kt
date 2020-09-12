c2a7c906c4ff11acf74d3ad25c36f9836cb7b313
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-09-12 17:04:50 +0900

    Add external url link

--- app/src/main/java/com/kwang0/introduce/common/Const.kt
+++ app/src/main/java/com/kwang0/introduce/common/Const.kt
@@ -2,4 +2,9 @@ package com.kwang0.introduce.common
 
 object Const {
     const val TAG = "Introduce kwang0"
+
+    const val EXTERNAL_BLOG_LINK = "https://wellohorld.tistory.com"
+    const val EXTERNAL_GITHUB_LINK = "https://github.com/KWANGYOUNGCHOI"
+    const val EXTERNAL_PLAY_STORE_LINK = "https://play.google.com/store/apps/developer?id=kwang0"
+
 }
